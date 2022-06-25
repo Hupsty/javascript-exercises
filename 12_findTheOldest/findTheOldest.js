525852ee0f742fc9b91b9b45210754ba5c602bb9
Hupsty <mail4hupy@aol.com>
2022-06-25 13:45:05 -0500

    fixed with reduce function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,23 +1,16 @@
 const findTheOldest = function(people) {
-    let oldestPerson;
-    let oldestAge = 0;
-    for (person of people) {
-        if (!person.yearOfDeath) {
-            const d = (new Date()).getFullYear();
-            const age = d - person.yearOfBirth;
-            if (age > oldestAge) {
-                oldestAge = age;
-                oldestPerson = person;
-                continue;
-            }
-        }
-        if (person.yearOfDeath - person.yearOfBirth > oldestAge) {
-            oldestAge = person.yearOfDeath - person.yearOfBirth;
-            oldestPerson = person;
-        }
-    }
-    return oldestPerson;
+    return people.reduce(function (oldest, current) {
+        const oldestAge = theirAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = theirAge(current.yearOfBirth, current.yearOfDeath);
+        return currentAge > oldestAge ? current : oldest;
+    })
 };
+const theirAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
